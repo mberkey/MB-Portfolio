9e4d23add35b80554f89ab5684db26c0286940eb
Matt Berkey <matthew.berkey@hotmail.com>
2022-02-17 17:28:07 -0500

    css update

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -4,7 +4,7 @@ export default class Hello extends Component {
     render(){        
            return(
             <>        
-                <div className="jumbotron m-9 p-5 mb-4 fluid text-end">
+                <div className="jumbotron m-9 p-5 mb-4 fluid text-align-right">
                     <div className="col-md-12">
                         <div className="h-50 p-5 text-left hello">
                          <h1>Hey, I'm <b>Matt</b></h1>
