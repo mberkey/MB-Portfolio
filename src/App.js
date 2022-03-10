004b723a907beb93b71ec787b5f8ca20e1b26d3f
Matt Berkey <matthew.berkey@hotmail.com>
2022-03-10 09:27:18 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import MainLayout from './containers/MainLayout';
 import Footer from './components/Footer';
-import Hello from './components/Hello';
-import { useEffect } from 'react';
 function App() {
 
   return (
