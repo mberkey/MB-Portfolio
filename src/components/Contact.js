24c2fff0ae306782818a2ea47937873a7f7fdf25
Matt Berkey <matthew.berkey@hotmail.com>
2022-03-10 10:37:03 -0500

    Contact update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,54 +1,37 @@
 import React, {Component} from "react";
 
-import { Container,Row, Col } from "react-bootstrap";
+import { Row, Container, Col } from "react-bootstrap";
 
 const cryptoJobLink = "https://cryptojobslist.com/talent/profile/crypto-professional-remote-pccj9fvkpb";
 const gitHubLink = "https://github.com/mberkey?tab=repositories";
 
 export default class Contact extends Component {
     render(){
-        return(
-
-            <div className="jumbotron m-6 p-2 mb-1 fluid rounded-3 text-center">
+        return(        
+            
+        <div className="jumbotron m-6 p-2 mb-1 fluid rounded-3 text-center">
             <div className="col-md-12">
                 <div className="h-80 p-3 contact">
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                        <Row className="justify-content-md-center">
-                            <Col md="auto">
-                                <h2>Contact Via <b>Email</b></h2>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col md="auto">
-                                <a href="mailto:matthew.berkey@hotmail.com" className='contactLink'>matthew.berkey@hotmail.com</a>                    
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">                            
-                            <Col md="auto">
-                                <h3>OR</h3>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col md="auto">
-                                <a href={cryptoJobLink} target="_blank" className='contactLink' rel="noopener">Hire me on <b>Crypto Jobs</b> List</a>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col md="auto">
-                                <h3>ALSO</h3>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col md="auto">
+                <Container>
+                <Row>                    
+                    <Col><h2>Contact <b>Me</b></h2></Col>
+                    <Col style={{disply:'flex', align:'start'}}> 
+                        <Row> 
+                            <Col>
+                                <a href="mailto:matthew.berkey@hotmail.com" className='contactLink'>matthew.berkey@hotmail.com</a>
+                                
+                                <a href={cryptoJobLink} target="_blank" className='contactLink' >Hire me on <b>Crypto Jobs</b> List</a>
+                            
                                 <a href={gitHubLink}  target="_blank" className='contactLink' rel="noopener">Yes, I have a <b>GitHub</b></a>
                             </Col>
                         </Row>
-                 </div>
-                 
+                   </Col>
+                </Row>
+                </Container>
+           
+                        
+                
+                </div>                 
             </div> 
         </div>
   
